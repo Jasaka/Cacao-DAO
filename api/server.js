434ff190c98173e56fcbf7b5e1ea087f5e1b77ab
Jan Samak <jan_karim.samak@smail.th-koeln.de>
2022-05-16 22:25:43 +0200

    feat(api): added `post` routes

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import multer from 'multer'
 import pkg from 'pg';
 import * as dbQueries from './dbQueries.mjs'
 import * as arweaveQueries from './arweaveQueries.mjs'
 import * as crypto from "crypto";
+import * as uuid from "uuid";
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 const {Pool} = pkg;
 
 const upload = multer();
@@ -39,28 +41,41 @@ const pool = () => {
 
 server.use(urlParser);
 server.use(jsonParser);
+server.use(cookieParser());
 
 server.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-server.post('/login', async (req, res) => {
-    const {userName, password} = req.body;
-    const userId = getIdFromLoginData(userName, password)
-    if (userId !== null) {
-        const sessionId = crypto.randomBytes(16).toString('hex');
-        const expirationDate = new Date(Date.now() + 5 * 60 * 60 * 1000);
 
-        pool().query(dbQueries.initiateNewSession, [sessionId, userId, expirationDate])
-            .then(res => {
-                res.cookie('sessionId', sessionId, {expires: expirationDate});
-            })
-            .catch(err => {
-                console.error(err);
-                res.status(500).json({error: err.message});
-            });
+server.post('/login', async (req, res) => {
+    let cookie = req.cookies.cookie;
+
+    if (cookie === undefined) {
+        const {userName, password} = req.body;
+        const userId = await getIdFromLoginData(userName, password)
+        if (userId !== null) {
+            const sessionId = crypto.randomBytes(16).toString('hex');
+            const expirationDate = new Date(Date.now() + 5 * 60 * 60 * 1000);
+
+            pool().query(dbQueries.initiateNewSession, [sessionId, userId, expirationDate])
+                .then(() => {
+                    res.cookie('cookie', sessionId, {expires: expirationDate, httpOnly: true});
+                    res.send('Check your cookies. One should be in there now');
+                })
+                .catch(err => {
+                    console.error(err);
+                    res.status(500).json({error: err.message});
+                });
+        } else {
+            res.status(401).json({error: 'Invalid username or password'});
+        }
     } else {
-        res.status(401).json({error: 'Invalid username or password'});
+        if (await isSessionValid(cookie)) {
+            res.send('Session is valid');
+        } else {
+            res.status(401).json({error: 'Invalid session'});
+        }
     }
 });
 
@@ -75,7 +90,6 @@ server.get('/users', async (req, res) => {
         });
 });
 
-// get user by id
 server.get('/users/:id', async (req, res) => {
     const id = req.params.id;
     pool().query(dbQueries.getUserById, [id])
@@ -88,7 +102,6 @@ server.get('/users/:id', async (req, res) => {
         });
 });
 
-// get proposals
 server.get('/proposals', async (req, res) => {
     pool().query(dbQueries.getProposals)
         .then(result => {
@@ -100,52 +113,31 @@ server.get('/proposals', async (req, res) => {
         });
 });
 
-function getUserIdFromSession(token) {
-    return pool().query(dbQueries.validateSession, [token])
-        .then(result => {
-            if (result.rows.length === 0) {
-                return null;
-            } else {
-                const expirationDate = new Date(result.rows[0].expirationDate);
-                if (expirationDate < new Date()) {
-                    pool().query(dbQueries.deleteSession, [token])
-                        .catch(err => {
-                            console.error(err);
-                        });
-                    return null;
-                } else {
-                    return result.rows[0].userId;
-                }
-            }
-        })
-        .catch(err => {
-            console.error(err);
-            return null;
-        });
-}
-
-// post new proposal with json body {title, description, predictedCost}
 server.post('/proposals', upload.none(), async (req, res) => {
-    const {title, description, predictedCost} = req.body;
-    const userId = getUserIdFromSession(req.headers.token);
-    const proposalId = generateUUId();
-    const proposalHash = generateProposalHash(proposalId, title, description, predictedCost);
-    if (userId !== null) {
-        pool().query(dbQueries.createProposal, [proposalId, title, description, predictedCost, proposalHash])
-            .then(result => {
-                res.json(result.rows);
-            })
-            .catch(err => {
-                console.error(err);
-                res.status(500).json({error: err.message});
-            });
+    let cookie = req.cookies.cookie;
+
+    if (await isSessionValid(cookie)) {
+        const {title, description, predictedCost} = req.body;
+        const userId = getUserIdFromSession(req.headers.token);
+        const proposalId = generateUUId();
+        const proposalHash = generateProposalHash(proposalId, title, description, predictedCost);
+        if (userId !== null) {
+            pool().query(dbQueries.createProposal, [proposalId, title, description, predictedCost, proposalHash])
+                .then(result => {
+                    res.json(result.rows);
+                })
+                .catch(err => {
+                    console.error(err);
+                    res.status(500).json({error: err.message});
+                });
+        } else {
+            res.status(401).json({error: 'Invalid session'});
+        }
     } else {
         res.status(401).json({error: 'Invalid session'});
     }
 });
 
-
-// get proposal by id
 server.get('/proposals/:id', async (req, res) => {
     const id = req.params.id;
     pool().query(dbQueries.getProposalById, [id])
@@ -170,23 +162,31 @@ server.get('/proposals/:id/flags', async (req, res) => {
         });
 });
 
-// post new flag with json body {proposalHash, flagMessage, proposalId, flagAuthorId}
 server.post('/proposals/:id/flags', upload.none(), async (req, res) => {
-    const {proposalHash, flagMessage, proposalId, flagAuthorId} = req.body;
-    const userId = getUserIdFromSession(req.headers.token);
-    let authorId = null;
-    if (flagAuthorId === true) {
-        authorId = userId;
-    }
-    if (userId !== null) {
-        pool().query(dbQueries.createProposalFlag, [generateUUId(), proposalHash, flagMessage, proposalId, authorId])
-            .then(result => {
-                res.json(result.rows);
-            })
-            .catch(err => {
-                console.error(err);
-                res.status(500).json({error: err.message});
-            });
+    let cookie = req.cookies.cookie;
+
+    if (await isSessionValid(cookie)) {
+        console.log(req.body);
+        const proposalId = req.params.id;
+        const {proposalHash, flagMessage, providesFlagAuthorId} = req.body;
+        const userId = await getUserIdFromSession(cookie);
+        console.log("proposalId: " + proposalId + " userId: " + userId + " proposalHash: " + proposalHash + " flagMessage: " + flagMessage + " flagAuthorId: " + providesFlagAuthorId);
+        let authorId = null;
+        if (providesFlagAuthorId === true) {
+            authorId = userId;
+        }
+        if (userId !== null) {
+            pool().query(dbQueries.createProposalFlag, [generateUUId(), proposalHash, flagMessage, proposalId, authorId])
+                .then(result => {
+                    res.json(result.rows);
+                })
+                .catch(err => {
+                    console.error(err);
+                    res.status(500).json({error: err.message});
+                });
+        } else {
+            res.status(401).json({error: 'Invalid session'});
+        }
     } else {
         res.status(401).json({error: 'Invalid session'});
     }
@@ -195,6 +195,7 @@ server.post('/proposals/:id/flags', upload.none(), async (req, res) => {
 server.get('/proposalFlags', async (req, res) => {
     pool().query(dbQueries.getProposalFlags)
         .then(result => {
+            console.log(result.rows);
             res.json(result.rows);
         })
         .catch(err => {
@@ -205,6 +206,45 @@ server.get('/proposalFlags', async (req, res) => {
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
+async function isSessionValid(cookie) {
+    return new Promise((resolve, reject) => {
+        pool().query(dbQueries.validateSession, [cookie], (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (result.rows.length === 0) {
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            }
+        });
+    });
+}
+
+function getUserIdFromSession(token) {
+    return pool().query(dbQueries.validateSession, [token])
+        .then(result => {
+            if (result.rows.length === 0) {
+                return null;
+            } else {
+                const expirationDate = new Date(result.rows[0].expirationDate);
+                if (expirationDate < new Date()) {
+                    pool().query(dbQueries.deleteSession, [token])
+                        .catch(err => {
+                            console.error(err);
+                        });
+                    return null;
+                } else {
+                    return result.rows[0].userId;
+                }
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            return null;
+        });
+}
 
 function getIdFromLoginData(username, password) {
     const passwordHash = generateHash(password);
@@ -242,5 +282,5 @@ function generateHash(data) {
 }
 
 function generateUUId() {
-    return uuidv4();
+    return uuid.v4();
 }
