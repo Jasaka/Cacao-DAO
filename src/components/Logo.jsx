8920ea5b7978a9b12cae5b8c6a8a932e878e8b0f
Jan Samak <jan_karim.samak@smail.th-koeln.de>
2022-12-21 05:37:47 +0100

    fix: logo path

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 export function Logo() {
   return (
     <div className="flex flex-row justify-center mx-auto">
-      <img src="/logo.svg" className="h-10 w-auto" />
+      <img src="./logo.svg" className="h-10 w-auto" />
       <span className="my-auto pl-1 text-xl font-display">
         Cacao<span className="text-[#72C187] font-semibold">DAO</span>
       </span>
